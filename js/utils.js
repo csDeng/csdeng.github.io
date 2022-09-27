9fa7f758451986d6f7808c1b6ca27ad04d7d7b30
CSJerry <553537528@qq.com>
2022-09-27 16:50:29 +0800

    hexo deploy

--- js/utils.js
+++ js/utils.js
@@ -1,167 +1,46 @@
-(function ($) {
-  "use strict";
+function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this
+    var args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+};
 
-  ZHAOO.utils = {
-    debounce: function (func, wait, immediate) {
-      var timeout;
-      return function () {
-        var context = this;
-        var args = arguments;
-        timeout && clearTimeout(timeout);
-        if (immediate) {
-          var callNow = !timeout;
-          timeout = setTimeout(function () {
-            timeout = null;
-          }, wait);
-          if (callNow) func.apply(context, args);
-        } else {
-          timeout = setTimeout(function () {
-            func.apply(context, args);
-          }, wait);
-        }
-      }
-    },
-    throttle: function (func, wait, options) {
-      var timeout, context, args;
-      var previous = 0;
-      if (!options) options = {};
-      var later = function () {
-        previous = options.leading === false ? 0 : new Date().getTime();
-        timeout = null;
-        func.apply(context, args);
-        if (!timeout) context = args = null;
-      }
-      var throttled = function () {
-        var now = new Date().getTime();
-        if (!previous && options.leading === false) previous = now;
-        var remaining = wait - (now - previous);
-        context = this;
-        args = arguments;
-        if (remaining <= 0 || remaining > wait) {
-          if (timeout) {
-            clearTimeout(timeout);
-            timeout = null;
-          }
-          previous = now;
-          func.apply(context, args);
-          if (!timeout) context = args = null;
-        } else if (!timeout && options.trailing !== false) {
-          timeout = setTimeout(later, remaining);
-        }
-      }
-      return throttled;
-    },
-    hasMobileUA: function () {
-      var nav = window.navigator;
-      var ua = nav.userAgent;
-      var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
-      return pa.test(ua);
-    },
-    isTablet: function () {
-      return (
-        window.screen.width > 767 &&
-        window.screen.width < 992 &&
-        this.hasMobileUA()
-      );
-    },
-    isMobile: function () {
-      return window.screen.width < 767 && this.hasMobileUA();
-    },
-    isDesktop: function () {
-      return !this.isTablet() && !this.isMobile();
-    },
-    isDuringDate: function (beginDateStr, endDateStr) {
-      var curDate = new Date(),
-        beginDate = new Date(beginDateStr),
-        endDate = new Date(endDateStr);
-      if (curDate >= beginDate && curDate <= endDate) {
-        return true;
-      }
-      return false;
-    },
-    bindKeyup: function (code, fn) {
-      $(document).keyup(function (e) {
-        var key = e.which || e.keyCode;;
-        if (key == code) {
-          fn();
-        }
-      });
+function throttle(func, wait, mustRun) {
+  var timeout
+  var startTime = new Date()
+
+  return function () {
+    var context = this
+    var args = arguments
+    var curTime = new Date()
+
+    clearTimeout(timeout)
+    if (curTime - startTime >= mustRun) {
+      func.apply(context, args)
+      startTime = curTime
+    } else {
+      timeout = setTimeout(func, wait)
     }
   }
+};
 
-  ZHAOO.zui = {
-    message: function ({ text, type, delay }) {
-      var message = '<div class="zui-message ' + (type || "info") + '"><p>' + text + '</p></div>';
-      $("body").append(message);
-      var e = $(".zui-message");
-      e.ready(function () {
-        e.addClass("in");
-        setTimeout(function () {
-          e.removeClass("in");
-          e.on("transitionend webkitTransitionEnd", function () {
-            $(this).remove();
-          });
-        }, delay || 3000);
-      });
-    },
-    notification: function ({ title, content, type, delay }) {
-      var storage = JSON.parse(localStorage.getItem("notification-closed"));
-      if (storage && storage.indexOf(title) >= 0) return;
-      var notification = '<div class="zui-notification ' + (type || "info") + '"><span>' + title + '</span><p>' + content + '</p><i class="j-notification-close iconfont iconbaseline-close-px"></i></div>';
-      $("body").append(notification);
-      var e = $(".zui-notification");
-      var close = $(".j-notification-close");
-      e.ready(function () {
-        e.addClass("in");
-        setTimeout(function () {
-          e.removeClass("in");
-          e.on("transitionend webkitTransitionEnd", function () {
-            $(this).remove();
-          });
-        }, delay || 3000);
-        close.on("click", function () {
-          e.removeClass("in");
-          if (storage) {
-            (storage.indexOf(title) < 0) && localStorage.setItem("notification-closed", JSON.stringify(storage.concat(title)));
-          } else {
-            localStorage.setItem("notification-closed", JSON.stringify([title]));
-          }
-        });
-      });
-    }
-  }
+function isMobile() {
+  var check = false;
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera)
+  return check
+};
 
-})(jQuery);
+window.debounce = debounce
 
-class AsyncLimit {
-  constructor(limit) {
-    this.limit = Number(limit) || 2;
-    this.pool = [];
-    this.current = 0;
-  }
+window.throttle = throttle
 
-  async run(fn) {
-    if (!fn || typeof fn !== 'function') {
-      throw new Error('Function error.');
-    }
-    if (this.current >= this.limit) {
-      await new Promise(resolve => this.pool.push(resolve));
-    }
-    return this._handleRun(fn);
-  }
-
-  async _handleRun(fn) {
-    this.current++;
-    try {
-      return await fn();
-    } catch (err) {
-      return Promise.reject(err);
-    } finally {
-      this.current--;
-      if (this.pool.length) {
-        this.pool[0]();
-        this.pool.shift();
-      }
-    }
-  }
-}
+window.isMobile = isMobile
